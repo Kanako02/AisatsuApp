56bc484f6d441fd44db38d3584ccd9a9141b65a3
ka-takahashi <ka-takahashi@i-enter.co.jp>
2020-04-13 18:32:51 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/kanako/aisatsuapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package jp.techacademy.kanako.aisatsuapp
+
+import android.app.TimePickerDialog
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener {
+            val timePickerDialog = TimePickerDialog(
+                this,
+                TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                    Log.d("UI_PARTS", "$hour:$minute")
+                },
+                13, 0, true
+            )
+
+            timePickerDialog.show()
+        }
+    }
+}
