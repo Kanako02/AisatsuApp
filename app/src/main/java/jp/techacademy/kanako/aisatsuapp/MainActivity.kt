92ca09fd5f9469bf380299c397a77e6bdadf2cb3
ka-takahashi <ka-takahashi@i-enter.co.jp>
2020-04-14 14:44:43 +0900

    aisatsu

--- app/src/main/java/jp/techacademy/kanako/aisatsuapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/kanako/aisatsuapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package jp.techacademy.kanako.aisatsuapp
 
 import android.app.TimePickerDialog
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -14,15 +15,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button1.setOnClickListener {
-            val timePickerDialog = TimePickerDialog(
-                this,
-                TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+            val timePickerDialog = TimePickerDialog(this, TimePickerDialog.OnTimeSetListener { view, hour, minute ->
                     Log.d("UI_PARTS", "$hour:$minute")
+
+                if(2 <= hour && hour < 10 ){
+                    textView.text="おはよう"}
+                    else if (10 <= hour && hour < 18){
+                    textView.text="こんにちは"}
+                    else textView.text = "こんばんわ"
+
+
                 },
-                13, 0, true
+                1, 0, true
             )
 
             timePickerDialog.show()
+
         }
     }
+
 }
+
+//画面内にButtonとTextViewを配置してください。
+//ButtonをタッチするとTimePickerDialogを表示して，設定した時刻に応じてTextViewに表示されるあいさつを変化させてください。
+//2:00 ~ 9:59 では「おはよう」
+//10:00 ~ 17:59 では「こんにちは」
+//18:00 ~ 1:59 では「こんばんは」
+//ヒント
+//TimePickerDialogのインスタンスのhourとminuteから時刻と分を取得
